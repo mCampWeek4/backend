712f0a8b080ac1d041884b900f9a9eb7e5e09ef1
seungkukim <seungkukim@kaist.ac.kr>
2022-01-24 05:02:38 +0000

    before moving

--- routes/recipe.js
+++ routes/recipe.js
@@ -5,6 +5,35 @@ const sequelize = require('sequelize');
 
 const router = express.Router();
 
+// const getRecipe = async(req, res) => {
+//     try {
+//         const str = req.params.ingredientIdString;
+//         const myIngredients = str.split('.');
+//         const len = parseInt(myIngredients, 10)
+//         console.log(len);
+
+//         const matchedRecipe = await RecipeIngredient.findAll({
+//             attributes: ['descriptionIdRecipe', [sequelize.fn('COUNT', sequelize.col('ingredientIdRecipe')), 'total']],
+//             where: {
+//                 ingredientIdRecipe: {
+//                     [Op.in]: myIngredients
+//                 }
+
+//             },
+//             group: ['descriptionIdRecipe'],
+//             order: [
+//                 [sequelize.fn('COUNT', sequelize.col('ingredientIdRecipe')), 'DESC']
+//             ],
+//             limit: 30
+//         })
+
+//         console.log(matchedRecipe);
+//         res.send(matchedRecipe);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+
 const getRecipe = async(req, res) => {
     try {
         const str = req.params.ingredientIdString;
@@ -12,49 +41,29 @@ const getRecipe = async(req, res) => {
         const len = parseInt(myIngredients, 10)
         console.log(len);
 
-        const deficientRecipe = await RecipeIngredient.findAll({
+        const matchedRecipe = await RecipeIngredient.findAll({
             attributes: ['descriptionIdRecipe', [sequelize.fn('COUNT', sequelize.col('ingredientIdRecipe')), 'total']],
             where: {
                 ingredientIdRecipe: {
                     [Op.in]: myIngredients
+                },
+                include: {
+                    model: model.ProductCategory,
+                    where: {
+
+                    }
                 }
+
             },
             group: ['descriptionIdRecipe'],
-            // order: sequelize.fn('COUNT', sequelize.col('ingredientIdRecipe'))
             order: [
                 [sequelize.fn('COUNT', sequelize.col('ingredientIdRecipe')), 'DESC']
-            ]
+            ],
+            limit: 30
         })
 
-
-
-        // const deficientRecipe = await RecipeIngredient.findAndCountAll({
-        //     attributes: ['descriptionIdRecipe'],
-        //     where: {
-        //         ingredientIdRecipe: {
-        //             [Op.notIn]: myIngredients
-        //         }
-        //     },
-        //     group: ['descriptionIdRecipe'],
-        // })
-
-
-        // const deficientRecipe = await RecipeIngredient.count({
-        //     distinct: 'descriptionIdRecipe',
-        //     where: {
-        //         ingredientIdRecipe: {
-        //             [Op.notIn]: myIngredients
-        //         }
-        //     }
-        // }).then(function(count) {
-        //     return {
-
-        //     }
-        // });
-
-
-        console.log(deficientRecipe);
-        res.send(deficientRecipe);
+        console.log(matchedRecipe);
+        res.send(matchedRecipe);
     } catch (err) {
         console.error(err);
     }
